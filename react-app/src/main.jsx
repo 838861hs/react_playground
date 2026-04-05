d3dd4c97923c0427836b3d3dbf49915dd365cc56
838861hs <philoaki0@gmail.com>
2026-04-06 00:08:03 +0900

    不要設定削除

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,7 +2,6 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from "./App";
 import Counter from "./Counter"
-import UseEffectCounter from './useEffectCounter'
 
 createRoot(document.getElementById('root')).render(
   <>
