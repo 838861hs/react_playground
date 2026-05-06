8cfdaff8b7f316c18f15e82cbe811bdea6d3b18f
838861hs <philoaki0@gmail.com>
2026-05-06 12:41:04 +0900

    共通設定cssを追加

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import Header from './Header.jsx';
+import './scss/main.scss'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
